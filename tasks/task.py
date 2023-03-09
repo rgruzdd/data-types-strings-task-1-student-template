340e63f3c26a959812cdf629dd82120ef2b66878
Gruzin <gruzd.roman@inbox.ru>
2023-03-09 16:40:44 +0300

    Commit with a solution

--- tasks/task.py
+++ tasks/task.py
@@ -1,5 +1,16 @@
 def get_fractions(a_b: str, c_b: str) -> str:
     """
-    Add your code here  
+    Add your code here
     """
-    return ''
+    a = a_b.split('/')[0]
+    c = c_b.split('/')[0]
+    sum = int(a) + int(c)
+    result = a_b + ' + ' + c_b + ' = ' + str(sum) + '/' + a_b.split('/')[1]
+    return result
+
+a_b = '1/3'
+c_b = '5/3'
+
+print(get_fractions(a_b, c_b))
+
+
